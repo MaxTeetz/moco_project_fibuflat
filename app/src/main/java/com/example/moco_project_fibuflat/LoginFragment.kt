fd45c2a0b8a837cb11e6ab3e62b85b602b692091
maxteetz <max-julien@hotmail.de>
2022-01-12 19:52:50 +0100

    navigate to register

--- app/src/main/java/com/example/moco_project_fibuflat/LoginFragment.kt
+++ app/src/main/java/com/example/moco_project_fibuflat/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.moco_project_fibuflat.databinding.FragmentLoginBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,25 +22,35 @@ class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
 
     private var _binding : FragmentLoginBinding? = null
-    private val binding get() = _binding
+    private val binding get() = _binding!!
     
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    //override fun onCreate(savedInstanceState: Bundle?) {
+    //    super.onCreate(savedInstanceState)
+    //    arguments?.let {
+    //        param1 = it.getString(ARG_PARAM1)
+    //        param2 = it.getString(ARG_PARAM2)
+    //    }
+    //}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.createAccountText.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+            this.findNavController().navigate(action)
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/moco_project_fibuflat/RegisterFragment.kt
+++ app/src/main/java/com/example/moco_project_fibuflat/RegisterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.moco_project_fibuflat.databinding.FragmentRegisterBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,24 +19,37 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    //override fun onCreate(savedInstanceState: Bundle?) {
+    //    super.onCreate(savedInstanceState)
+    //    arguments?.let {
+    //        param1 = it.getString(ARG_PARAM1)
+    //        param2 = it.getString(ARG_PARAM2)
+    //    }
+    //}
+//
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //binding.login.setOnClickListener {
+            //val action = RegisterFragmentDirections.actionRegisterFragmentToLoginFragment()
+            //findNavController().navigate(action)
+        //}
     }
 
     companion object {
