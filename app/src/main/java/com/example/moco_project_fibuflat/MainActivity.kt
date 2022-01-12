e502d785cc38e71913c59ccba3cc6cdadee2de99
maxteetz <max-julien@hotmail.de>
2022-01-12 19:26:53 +0100

    gradle dependencies etc.

--- app/src/main/java/com/example/moco_project_fibuflat/MainActivity.kt
+++ app/src/main/java/com/example/moco_project_fibuflat/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.moco_project_fibuflat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
